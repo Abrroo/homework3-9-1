9b302f813855eb536e46d866344e590d61d6f1b9
Ersyn <akhmetov.ersyn@gmail.com>
2023-03-25 20:57:26 +0600

    gg

--- homework3-9-1/homework3-9-1.cpp
+++ homework3-9-1/homework3-9-1.cpp
@@ -12,22 +12,30 @@ public:
 		numerator_ = numerator;
 		denominator_ = denominator;
 	}
+	int multiplyFirstCross(Fraction other) { return numerator_ * other.denominator_; }
+	int multiplySecondCross(Fraction other) { return other.numerator_ * denominator_; }
 
-	bool operator==(Fraction other) { numerator_ == denominator_; }
+	bool operator==(Fraction other) { return numerator_ == other.numerator_ && denominator_ == other.denominator_; }
+	bool operator!=(Fraction other) { return !(numerator_ == other.numerator_ && denominator_ == other.denominator_); }
+	bool operator<(Fraction other) { return multiplyFirstCross(other) < multiplySecondCross(other); }
+	bool operator>(Fraction other) { return multiplyFirstCross(other) > multiplySecondCross(other); }
+	bool operator<=(Fraction other) { return !(multiplyFirstCross(other) > multiplySecondCross(other)); }
+	bool operator>=(Fraction other) { return !(multiplyFirstCross(other) < multiplySecondCross(other)); }
+
+
 };
 
 int main()
 {
-	Fraction f1(4, 3);
-	Fraction f2(6, 11);
+	Fraction f1(5, 3);
+	Fraction f2(4, 3);
 
 	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << '\n';
-	/*
 	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << '\n';
 	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << '\n';
 	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << '\n';
 	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << '\n';
 	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << '\n';
-	*/
+	
 	return 0;
 }
